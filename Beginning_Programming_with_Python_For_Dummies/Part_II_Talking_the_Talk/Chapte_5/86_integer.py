8ca86a36b9db8619c53dfd3eb931075920f4db26
Mobarak <mobarak.ali@gmail.com>
2015-09-10 00:36:50 +0600

    C13 : Part II => Chapter_5
    work progress to 87page

---
+++ Beginning_Programming_with_Python_For_Dummies/Part_II_Talking_the_Talk/Chapte_5/86_integer.py
@@ -0,0 +1,33 @@
+__author__ = 'Mobarak'
+
+
+#Page:86
+################################
+# Variable
+################################
+
+
+b1  = 0b10 # Base 1
+b8  = 0o10 # Base 8
+b10 = 10   # Base 10
+b16 = 0x10 # Base 16
+
+print(b1,  b8, b10, b16)
+#output with base 10 : 2,8,10,16
+
+v1 = 75
+
+# to binary
+n_binary = bin(v1)
+print(n_binary)
+
+# to octal
+n_octal = oct(v1)
+print(n_octal)
+
+# to hexadecimal
+n_hex = hex(v1)
+print(n_hex)
+
+
+
---
+++ Beginning_Programming_with_Python_For_Dummies/Part_II_Talking_the_Talk/Chapte_5/Chapter 5 Storing and Modifying Information.py
@@ -0,0 +1,31 @@
+"""
+In This Chapter
+? Understanding data storage
+? Considering the kinds of data storage
+? Adding dates and times to applications
+
+# page:84
+=====================================
+Using the right box to store the data
+=====================================
+
+just keep in mind that each kind of information is stored in a special kind
+of variable.
+
+However,
+computers don’t actually know about information types. All that the computer
+knows about are 0s and 1s, which is the absence or presence of a voltage.
+
+
+Numbers, letters, dates, times, and any other kind of information
+you can think about all come down to 0s and 1s in the computer system.
+
+----------------------------------------
+Defining the Essential Python Data Types
+========================================
+
+Every programming language defines variables that hold specific kinds of
+information, and Python is no exception. The specific kind of variable is
+called a data type.
+
+"""
