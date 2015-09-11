90e2729f450ad5641e71eb48cea25105444ea67e
Mobarak <mobarak.ali@gmail.com>
2015-09-12 01:30:05 +0600

    C14 : Working on Chapter 13
    Completed page 243 to 253

---
+++ Beginning_Programming_with_Python_For_Dummies/Part_III_Performing_Common_Tasks/Chappter_13/244_Understanding Collections.py
@@ -0,0 +1,10 @@
+__author__ = 'Mobarak'
+
+"""
+No matter which sequence you use, they all support
+two functions: index()and count().
+"""
+str = "Hi there! I'm a string"
+count = str.count("Hi")
+print("We have found the string \"string\"", count, "times.")
+
